14b7468d90686974211823a2ccbb4ee796a4bc4a
Tom Sydney Kerckhove <syd.kerckhove@student.kuleuven.be>
2013-11-01 21:20:18 +0100

    started on goingout script

---
+++ paula/goingout/goingout_script.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+##
+#      ____   _   _   _ _        _    
+#     |  _ \ / \ | | | | |      / \   
+#     | |_) / _ \| | | | |     / _ \  
+#     |  __/ ___ \ |_| | |___ / ___ \ 
+#     |_| /_/   \_\___/|_____/_/   \_\
+#
+#
+# Personal
+# Artificial
+# Unintelligent
+# Life
+# Assistant
+#
+##
+
+from paula.paula import PAULA
+from . import goingout_config as conf
+
+def execute():
+    p = Paula()
+    
+    print("placeholder")
+

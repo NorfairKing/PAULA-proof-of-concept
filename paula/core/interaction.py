67aeb7e9cdf6662ff0c0e4aa68ff9f616e62579c
Tom Sydney Kerckhove <syd.kerckhove@student.kuleuven.be>
2013-12-11 11:53:09 +0100

    planning

--- paula/core/interaction.py
+++ paula/core/interaction.py
@@ -18,6 +18,7 @@
 from .command import decide
 from .speak import voice
 
+
 def decide_meaning(string):
     return decide.decide_meaning(string)
 
@@ -25,11 +26,14 @@ def decide_meaning(string):
 def means(string, meaning):
     return decide.means(string, meaning)
 
+
 def say(text):
     return voice.say(text)
 
+
 def print_error(error_string):
-    print("\033[1;31m" + "ERROR: " + error_string+" \033[0m")
+    print("\033[1;31m" + "ERROR: " + error_string + " \033[0m")
+
 
 def print_debug(debug_string):
-    print("\033[1;36m" + "DEBUG: " + debug_string+" \033[0m")
+    print("\033[1;36m" + "DEBUG: " + debug_string + " \033[0m")

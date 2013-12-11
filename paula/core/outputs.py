a14abb833c90fb0b436c36886f26032435e9f74b
Tom Sydney Kerckhove <syd.kerckhove@student.kuleuven.be>
2013-12-11 12:02:15 +0100

    refractored paula_outputs

--- paula/core/outputs.py
+++ paula/core/outputs.py
@@ -15,10 +15,12 @@
 #
 ##
 
-from . import paula_output_config as conf
+from .paula_output import string
+
+from . import  core_config
 
 def print_error(error_string):
-    print("\033[1;31m" + "ERROR: " + error_string+" \033[0m")
+    string.print_error(error_string)
 
 def print_debug(debug_string):
-    print("\033[1;36m" + "DEBUG: " + debug_string+" \033[0m")
+    string.print_debug(debug_string)

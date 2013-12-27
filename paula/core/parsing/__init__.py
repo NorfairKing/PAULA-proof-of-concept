2d2c17543098cfac538fff7df6c152871915910e
Tom Sydney Kerckhove <syd.kerckhove@student.kuleuven.be>
2013-12-27 17:24:29 +0100

    made files

---
+++ paula/core/parsing/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+##
+#      ____   _   _   _ _        _    
+#     |  _ \ / \ | | | | |      / \   
+#     | |_) / _ \| | | | |     / _ \  
+#     |  __/ ___ \ |_| | |___ / ___ \ 
+#     |_| /_/   \_\___/|_____/_/   \_\
+#
+#
+# Personal
+# Artificial
+# Unintelligent
+# Life
+# Assistant
+#
+##
+
---
+++ paula/core/parsing/parse_time.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+##
+#      ____   _   _   _ _        _    
+#     |  _ \ / \ | | | | |      / \   
+#     | |_) / _ \| | | | |     / _ \  
+#     |  __/ ___ \ |_| | |___ / ___ \ 
+#     |_| /_/   \_\___/|_____/_/   \_\
+#
+#
+# Personal
+# Artificial
+# Unintelligent
+# Life
+# Assistant
+#
+##
+
+def parse_delta(string):
+    pass
+
+def parse_datetime(string):
+    pass

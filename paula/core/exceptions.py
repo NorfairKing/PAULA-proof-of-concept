f15588c812cd1d1d1962c0aaf702f64fd8c70b54
Tom Sydney Kerckhove <syd.kerckhove@student.kuleuven.be>
2013-12-27 17:36:20 +0100

    started on exceptions

---
+++ paula/core/exceptions.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+##
+#      ____   _   _   _ _        _    
+#     |  _ \ / \ | | | | |      / \   
+#     | |_) / _ \| | | | |     / _ \  
+#     |  __/ ___ \ |_| | |___ / ___ \ 
+#     |_| /_/   \_\___/|_____/_/   \_\
+#
+#
+# Personal
+# Artificial
+# Unintelligent
+# Life
+# Assistant
+#
+##
+
+class PAULA_Exception(Exception):
+     def __init__(self, message="PAULA_Exception"):
+        Exception.__init__(self, message)

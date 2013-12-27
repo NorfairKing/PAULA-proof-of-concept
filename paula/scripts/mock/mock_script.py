7d4eba460d9852a9cb02a279d6b1dd9f7724f0a8
Tom Sydney Kerckhove <syd.kerckhove@student.kuleuven.be>
2013-12-28 00:33:44 +0100

    added mock script

---
+++ paula/scripts/mock/mock_script.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+##
+#      ____   _   _   _ _        _    
+#     |  _ \ / \ | | | | |      / \   
+#     | |_) / _ \| | | | |     / _ \  
+#     |  __/ ___ \ |_| | |___ / ___ \ 
+#     |_| /_/   \_\___/|_____/_/   \_\
+#
+#
+# Personal
+# Artificial
+# Unintelligent
+# Life
+# Assistant
+#
+##
+
+from paula.core import system
+
+from . import mock_script_config as conf
+
+
+def execute(operand):
+    cmd = ['vlc', '-Idummy', '--play-and-exit', '-vvv', conf.HAHA_FILE]
+    process = system.call_list_silently(cmd, sync=False)
---
+++ paula/scripts/mock/mock_script_config.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+##
+#      ____   _   _   _ _        _    
+#     |  _ \ / \ | | | | |      / \   
+#     | |_) / _ \| | | | |     / _ \  
+#     |  __/ ___ \ |_| | |___ / ___ \ 
+#     |_| /_/   \_\___/|_____/_/   \_\
+#
+#
+# Personal
+# Artificial
+# Unintelligent
+# Life
+# Assistant
+#
+##
+
+import os
+
+# Default = False
+DEBUG = True
+
+NEW_SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
+RESOURCES_DIR = os.path.join(NEW_SCRIPT_DIR, 'resources')
+HAHA_FILE = os.path.join(RESOURCES_DIR,"haha.mp3")

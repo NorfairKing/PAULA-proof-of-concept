54016d2eacceb79c97bcdd5e6259f12cd434a038
Tom Sydney Kerckhove <syd.kerckhove@student.kuleuven.be>
2013-12-28 23:40:17 +0100

    started on training script

---
+++ paula/scripts/training/training_script.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+##
+#      ____   _   _   _ _        _    
+#     |  _ \ / \ | | | | |      / \   
+#     | |_) / _ \| | | | |     / _ \  
+#     |  __/ ___ \ |_| | |___ / ___ \ 
+#     |_| /_/   \_\___/|_____/_/   \_\
+#
+#
+# Personal
+# Artificial
+# Unintelligent
+# Life
+# Assistant
+#
+##
+
+from paula.core import inputs
+
+from . import training_script_config as conf
+
+def execute(operand):
+    inputs.get_item_from_dict(conf.TRAINING_OPTIONS)
---
+++ paula/scripts/training/training_script_config.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+##
+#      ____   _   _   _ _        _    
+#     |  _ \ / \ | | | | |      / \   
+#     | |_) / _ \| | | | |     / _ \  
+#     |  __/ ___ \ |_| | |___ / ___ \ 
+#     |_| /_/   \_\___/|_____/_/   \_\
+#
+#
+# Personal
+# Artificial
+# Unintelligent
+# Life
+# Assistant
+#
+##
+
+import os
+
+# Default = False
+DEBUG = True
+
+TRAINING_OPTIONS = [
+    "room_training"
+]

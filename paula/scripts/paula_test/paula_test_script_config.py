1d6cbccc4d5e06aa4464092cf931a850232902e9
Tom Sydney Kerckhove <syd.kerckhove@student.kuleuven.be>
2014-01-05 09:28:22 +0100

    cleaning and fixing

--- paula/scripts/paula_test/paula_test_script_config.py
+++ paula/scripts/paula_test/paula_test_script_config.py
@@ -15,16 +15,4 @@
 #
 ##
 
-import os
-
-from paula.core import config
-
-DEBUG = True
-
-HERE = os.path.dirname(os.path.realpath(__file__))
-
-PAULA_TEST_SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
-RESOURCES_DIR = os.path.join(PAULA_TEST_SCRIPT_DIR, 'resources')
-DEFAULT_CONFIG_FILE = os.path.join(RESOURCES_DIR,'default_config_file')
-
-
+DEBUG = False

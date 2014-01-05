0827ec72daa5a31f8c3e386d73305e45abf3ebad
Tom Sydney Kerckhove <syd.kerckhove@student.kuleuven.be>
2014-01-05 09:26:58 +0100

    cleaning and fixing

--- paula/scripts/paula_test/paula_test_script.py
+++ paula/scripts/paula_test/paula_test_script.py
@@ -17,17 +17,11 @@
 
 from paula.scripts.script import Script
 
-from paula.core import outputs
-from paula.core import interaction
-
-from . import paula_test_script_config as conf
-
 
 class PaulaTestScript(Script):
-    def execute(self,operand):
-        if conf.DEBUG:
-            self.debug("The arguments to execute this script were the following.")
-            self.debug(operand)
+    def execute(self, operand):
+        self.debug("The arguments to execute this script were the following.")
+        self.debug(operand)
 
 
         # <Test here>
